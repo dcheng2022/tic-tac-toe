525e1e7c55b2d2749c4cf1941c24d405196052fb
Darin <darinchengg@gmail.com>
2022-06-21 15:16:09 -0500

    Write input_getter method for piece placement validation

--- tic-tac-toe.rb
+++ tic-tac-toe.rb
@@ -1,4 +1,5 @@
-game_board = Array.new(2) { Array.new(3) }
+require 'pry-byebug'
+game_board = Array.new(3) { Array.new(3, ' ') }
 
 class Player 
   @@total_num_of_pieces = 0
@@ -7,12 +8,34 @@ class Player
     @piece = piece
   end
 
-  def place_piece(pos)
+  def self.total_num_of_pieces
+    @@total_num_of_pieces
+  end
+
+  def place_piece
+    @@total_num_of_pieces += 1
+    piece
   end
 
   def player_win?
   end
 
   def game_draw? 
+  end
+
+  private
+
+  attr_reader :piece
+end
+
+def input_getter
+  puts 'Enter a position.'
+  loop do
+    input = gets.chomp.upcase.split('')
+    next unless input.length == 2 && ('A'..'C').include?(input[0]) && ('1'..'3').include?(input[1])
+
+    input[0] = input[0].ord - 65
+    input[1] = input[1].to_i - 1
+    return input
   end
 end
